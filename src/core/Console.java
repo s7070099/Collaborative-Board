578d9ae929df3342ebcbab00f3bbd69bbf0fae79
rnarktrs <mr.ts_00769@msn.com>
2015-11-09 14:36:41 +0700

    console update

--- src/core/Console.java
+++ src/core/Console.java
@@ -12,6 +12,19 @@ class Console extends Thread {
 			   System.out.println("[HELP]");
 			   System.out.println("/show paper [state] - Show or Hide Paper Window (0 or 1)");
 		   }
+		   
+		   if(input.next().equals("/load")){
+			   String filename = input.next();
+			   //load
+			   System.out.println("Load");
+		   }
+		   
+		   if(input.next().equals("/save")){
+			   String filename = input.next();
+			   //save
+			   System.out.println("Save");
+		   }
+		   //อันนี้เป็นคอลโซล ไว้ ดีบัก
 		   if(input.next().equals("/show")){
 			   if(input.next().equals("paper")){
 				   if(input.next().equals("1")){
