0bae7ed43b1373dd51c8bb6ce1acb8532a75c83e
rnarktrs <mr.ts_00769@msn.com>
2015-11-15 22:50:54 +0700

    Window login
    เพิ่งจะมาแก้ขนาด

--- src/gui/WindowLogin.java
+++ src/gui/WindowLogin.java
@@ -15,42 +15,40 @@ public class WindowLogin extends JFrame{
 		WindowLogin frame = new WindowLogin();
 		frame.setTitle("Collaborative - login");
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		frame.setBounds(100, 100, 310, 470);
+		frame.setBounds(0, 0, 1280, 720);
 		frame.setLayout(null);
 		
 		//component
-		JTextField user_input = new JTextField();
+		JTextField user_input = new JTextField("Username");
 		getContentPane().add(user_input);
-		/* Wait to add promt text in textfield*/
-		user_input.setBounds(50, 125, 200, 25);
+		user_input.setBounds(600, 375, 200, 25);
 		
-		JTextField pass_input = new JTextField();
+		JTextField pass_input = new JTextField("Server IP");
 		getContentPane().add(pass_input);
-		/* Wait to add promt text in textfield*/
-		pass_input.setBounds(50, 150, 200, 25);
+		pass_input.setBounds(600, 400, 200, 25);
 		
 		JButton login_button = new JButton("Login");
 		getContentPane().add(login_button);
 		login_button.setBackground(Color.white);
-		login_button.setBounds(50, 180, 200, 40);
+		login_button.setBounds(600, 430, 200, 40);
 		
 		JRadioButton radio1 = new JRadioButton("Login server automatical");
 		getContentPane().add(radio1);
 		radio1.setSelected(true);
-		radio1.setBounds(50,230,200,20);
+		radio1.setBounds(600,480,200,20);
 		
 		JRadioButton radio2 = new JRadioButton("Remember server IP");
 		getContentPane().add(radio2);
-		radio2.setBounds(50,250,200,20);
+		radio2.setBounds(600,500,200,20);
 	
 		JButton signup_button = new JButton("Sign Up");
 		getContentPane().add(signup_button);
 		signup_button.setBackground(Color.white);
-		signup_button.setBounds(50, 300, 200, 40);
+		signup_button.setBounds(600, 550, 200, 40);
 		
 		JLabel forgot_pwd = new JLabel("Forgotten Your Password?");
 		getContentPane().add(forgot_pwd);
-		forgot_pwd.setBounds(75, 395, 200, 40);
+		forgot_pwd.setBounds(620, 645, 200, 40);
 		//show
 		frame.setVisible(true);
 	}
@@ -58,9 +56,9 @@ public class WindowLogin extends JFrame{
 	public void paint(Graphics g){
 		super.paint(g);
 		Graphics2D g2 = (Graphics2D) g;
-		Line2D line = new Line2D.Float(60,320,260,320);
+		Line2D line = new Line2D.Float(610,570,810,570);
 		g2.draw(line);
-		Line2D line2 = new Line2D.Float(0,430,300,430);
+		Line2D line2 = new Line2D.Float(0,680,1280,680);
 		g2.draw(line2);
 	}
 	
